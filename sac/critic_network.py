2c0b5a8e28d8ebfd6ae836f43d79d65aedd2618b
Eric Choi <ericchoi001@gmail.com>
2025-08-18 20:45:39 +0900

    version 1

---
+++ sac/critic_network.py
@@ -0,0 +1,48 @@
+import os
+import torch as T
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.normal import Normal
+import numpy as np
+
+hyperparameters = {
+    'learning_rate': 0.0003,
+    'activation': F.relu,
+    'fc1_dims': 256,
+    'fc2_dims': 256,
+}
+
+class KHUDA_Finder_CriticNetwork(nn.Module):
+    def __init__(self, hparams, input_dims, n_actions,
+                 name='critic', chkpt_dir='critic_project/trial_1'):
+        super(KHUDA_Finder_CriticNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'1')
+
+        learning_rate = hparams['learning_rate']
+        optimizer_class = hparams['optimizer']
+        self.activation_fn = hparams['activation']
+        fc1_dims = hparams['fc1_dims']
+        fc2_dims = hparams['fc2_dims']
+
+        self.fc1 = nn.Linear(self.input_dims[0] + n_actions, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.q = nn.Linear(fc2_dims, 1)
+
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def forward(self, state, action):
+        action_value = self.fc1(T.cat([state, action], dim=1))
+        action_value = self.activation_fn(action_value)
+        action_value = self.fc2(action_value)
+        action_value = self.activation_fn(action_value)
+
+        q = self.q(action_value)
+
+        return q
